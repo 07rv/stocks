b869b349ea9574e31597715285a05ff73c94394c
Rohit Verma <28vrohit@gmail.com>
2023-07-24 22:06:20 +0530

    set-up

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Stock price</title>
+        <link rel="icon" href="/stock.ico" />
+      </Head>
+      <main></main>
+    </>
+  );
+}
