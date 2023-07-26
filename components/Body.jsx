b638adfb2465d155a970722e4c68de225221de84
Rohit Verma <28vrohit@gmail.com>
2023-07-26 22:12:48 +0530

    code changes

--- components/Body.jsx
+++ components/Body.jsx
@@ -1,7 +1,23 @@
-import Form from "./Form";
-import Stocks from "./Stocks";
+import {
+  Button,
+  TextField,
+  Grid,
+  Box,
+  Paper,
+  Container,
+  InputLabel,
+  Select,
+  MenuItem,
+  FormHelperText,
+  FormControl,
+} from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
-import { Box, Grid, Paper } from "@mui/material";
+import { useState } from "react";
+
+import { stocksList } from "@/data/Data";
+
+const defaultTheme = createTheme();
 import { styled } from "@mui/material/styles";
 
 const Item = styled(Paper)(({ theme }) => ({
@@ -14,18 +30,159 @@ const Item = styled(Paper)(({ theme }) => ({
 }));
 
 const Body = () => {
+  const [inputField, setInputField] = useState({
+    stock: "",
+    date: "",
+  });
+  const [errorField, setErrorField] = useState({
+    stock: "",
+    date: "",
+  });
+  const [stocksData, setStockData] = useState({
+    open: "",
+    High: "",
+    Low: "",
+    Close: "",
+    Volume: "",
+  });
+  const inputsHandler = (name, value) => {
+    setInputField((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+    setErrorField((prevState) => ({
+      ...prevState,
+      [name]: "",
+    }));
+  };
+  const setErrorMessage = (field, errorMessage) => {
+    setErrorField((prevState) => ({
+      ...prevState,
+      [field]: errorMessage,
+    }));
+  };
+  const checkAndSetValidationErrors = () => {
+    var hasError = false;
+    Object.keys(inputField).map((field) => {
+      if (field === "stock") {
+        if (inputField[field] === "") {
+          setErrorMessage(field, "Please Select Stock");
+          hasError = true;
+        }
+      } else if (field === "date") {
+        if (inputField[field] === "") {
+          setErrorMessage(field, "Please choose date");
+          hasError = true;
+        }
+      }
+    });
+    return hasError;
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (!checkAndSetValidationErrors()) {
+      const res = await fetch("/api/stock", {
+        method: "GET",
+      });
+      const data = await res.json();
+      setStockData(data);
+    }
+  };
   return (
     <Box sx={{ m: 2 }}>
       <Grid container spacing={2}>
         <Grid item xs={6} md={4}>
           <Item>
-            <Form />
+            <ThemeProvider theme={defaultTheme}>
+              <Container component="main" maxWidth="xs">
+                <Box
+                  sx={{
+                    marginTop: 2,
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    justifyContent: "center",
+                  }}
+                >
+                  <Box noValidate sx={{ mt: 3 }}>
+                    <Grid container spacing={2}>
+                      <Grid item xs={12}>
+                        <FormControl
+                          fullWidth
+                          sx={{ minWidth: 120 }}
+                          error={errorField && errorField.stock !== ""}
+                        >
+                          <InputLabel id="stock">Stocks</InputLabel>
+                          <Select
+                            labelId="stock"
+                            id="stockl"
+                            value={inputField.stock}
+                            label="Stock"
+                            name="stock"
+                            onChange={(e) =>
+                              inputsHandler("stock", e.target.value)
+                            }
+                          >
+                            <MenuItem value="">
+                              <em>None</em>
+                            </MenuItem>
+                            {stocksList.map((stock) => {
+                              return (
+                                <MenuItem
+                                  key={stock.id}
+                                  value={stock.stockTicker}
+                                >
+                                  {stock.company}
+                                </MenuItem>
+                              );
+                            })}
+                          </Select>
+                          {errorField && errorField.stock !== "" && (
+                            <FormHelperText>{errorField.stock}</FormHelperText>
+                          )}
+                        </FormControl>
+                      </Grid>
+                      <Grid item xs={12}>
+                        <FormControl
+                          fullWidth
+                          sx={{ minWidth: 120 }}
+                          error={errorField && errorField.date !== ""}
+                        >
+                          <TextField
+                            name="date"
+                            required
+                            fullWidth
+                            id="date"
+                            type="date"
+                            value={inputField.date}
+                            error={errorField && errorField.date !== ""}
+                            onChange={(e) =>
+                              inputsHandler(e.target.name, e.target.value)
+                            }
+                          />
+                          {errorField && errorField.date !== "" && (
+                            <FormHelperText>{errorField.date}</FormHelperText>
+                          )}
+                        </FormControl>
+                      </Grid>
+                    </Grid>
+                    <Button
+                      onClick={handleSubmit}
+                      fullWidth
+                      variant="contained"
+                      sx={{ mt: 3, mb: 2 }}
+                    >
+                      Sign In
+                    </Button>
+                  </Box>
+                </Box>
+              </Container>
+            </ThemeProvider>
           </Item>
         </Grid>
         <Grid item xs={6} md={8}>
-          <Item>
-            <Stocks />
-          </Item>
+          <Item>wert</Item>
         </Grid>
       </Grid>
     </Box>
--- pages/api/stock.js
+++ pages/api/stock.js
@@ -1,4 +1,7 @@
 export default function handler(req, res) {
   try {
+    res
+      .status(200)
+      .json({ open: 1234, High: 123, Low: 12, Close: 987654, Volume: 876543 });
   } catch (ex) {}
 }
