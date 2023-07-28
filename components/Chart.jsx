e11e1304aff21fbe2bac91a380f0dd81cb8139a0
Rohit Verma <28vrohit@gmail.com>
2023-07-28 12:57:31 +0530

    add chart com

--- components/Chart.jsx
+++ components/Chart.jsx
@@ -1,5 +1,53 @@
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
 const Chart = () => {
-  return <div>Chart</div>;
+  return (
+    <Line
+      options={{ responsive: true }}
+      data={{
+        labels: [
+          "January",
+          "February",
+          "March",
+          "April",
+          "May",
+          "June",
+          "July",
+          "Aug",
+          "Sep",
+          "Nov",
+        ],
+        datasets: [
+          {
+            label: "Dataset 1",
+            data: [1, 2, 3, 4, 5, 6, 7, 9, 13, 45],
+            borderColor: "black",
+            backgroundColor: "black",
+          },
+        ],
+      }}
+    />
+  );
 };
 
 export default Chart;
